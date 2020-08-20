aaf29d2357cf690089c18d24a58e79779ad7d07b
Daniel19902 <60512110+Daniel19902@users.noreply.github.com>
2020-08-20 09:58:06 -0500

    lab 3

---
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/AppTest.java
@@ -0,0 +1,21 @@
+package edu.eci.cvds.tdd;
+
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+
+    private void assertTrue(boolean b) {
+    }
+}
---
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -0,0 +1,7 @@
+package edu.eci.cvds.tdd.registry;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RegistryTest {
+}
